333997518b37d86480957a4052fd6633b3b46b8e
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2022-03-29 16:32:49 +0200

    Configured vars

--- js/vars.js
+++ js/vars.js
@@ -1,3 +1,17 @@
 
+const elma = document.querySelector('.var-a');
+const elmb = document.querySelector('.var-b');
+const elmc = document.querySelector('.var-c');
+
+
+
+// maak een variabele van het type getal
+
+// maak een string variabele
+
+// maak een boolean variabele
+
+
+
 
 
